934c9d2759902c16e881bb72e170977116f53c1d
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-01-29 19:58:25 +0900

    feat: Delete samples

--- app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ app/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -3,18 +3,37 @@ package jp.kaleidot725.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import jp.kaleidot725.sample.ui.sample.ViewModelFollowsNavBackStackEntryLifecycle
-import jp.kaleidot725.sample.ui.theme.DaggerHiltNavigationComposeTheme
+import jp.kaleidot725.sample.ui.screen.MainScreen
+import jp.kaleidot725.sample.ui.screen.SubScreen
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DaggerHiltNavigationComposeTheme {
-                // ViewModelDoesntFollowLifecycle()
-                ViewModelFollowsNavBackStackEntryLifecycle()
+            val navController = rememberNavController()
+            NavHost(navController, startDestination = "main") {
+                composable("main") {
+                    MainScreen(
+                        viewModel = hiltViewModel(),
+                        onNext = {
+                            navController.navigate("sub")
+                        }
+                    )
+                }
+                composable("sub") {
+                    SubScreen(
+                        viewModel = hiltViewModel(),
+                        onBack = {
+                            navController.popBackStack()
+                        }
+                    )
+                }
             }
         }
     }
